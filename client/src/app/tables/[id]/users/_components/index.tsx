fc1d7cbec43b369f41406c0fc29f51f1309f68ab
Kunal Dangi <kunaldangi252@gmail.com>
2024-07-10 22:17:40 +0530

    added addTableUser, removeTableUser UI feature

--- client/src/app/tables/[id]/users/_components/index.tsx
+++ client/src/app/tables/[id]/users/_components/index.tsx
@@ -1,18 +1,24 @@
 "use client";
 import "./style.css";
 
-import { Pencil, Save, Trash2, UserPlus, Users } from "lucide-react";
+import { CircleX, MailPlus, Pencil, Save, Trash2, UserPlus, Users } from "lucide-react";
 
 import { useEffect, useState } from "react";
 
 import { ErrorToast } from "@/components/ErrorToast";
 import { SuccessToast } from "@/components/SuccessToast";
 import { Switch } from "@/components/Switch";
+import Image from "next/image";
+import { ErrorBox } from "@/components/ErrorBox";
+import { SuccessBox } from "@/components/SuccessBox";
 
 export function TableUsers({url, id}: {url: string, id: string}){
     const [tableUsers, setTableUsers] = useState([] as any[]);
 
     const [edit, setEdit] = useState(false);
+
+    const [addError, setAddError] = useState(null);
+    const [addSuccess, setAddSuccess] = useState(null);
 
     const [tableUsersError, setTableUsersError] = useState(null);
     const [tableUsersSuccess, setTableUsersSuccess] = useState(null);
@@ -57,10 +63,10 @@ export function TableUsers({url, id}: {url: string, id: string}){
         }
         if(data.data.getTableUsers){
             data = data.data.getTableUsers;
-            console.log(data);
             data = [...data]
             setTableUsersSuccess("Table users fetched successfully!" as any);
             setTableUsers(data);
+            console.log(data);
         }
     }
 
@@ -105,12 +111,99 @@ export function TableUsers({url, id}: {url: string, id: string}){
             data = data.data.editTableUserPermissions;
             let newTableUsers = tableUsers; 
             newTableUsers[index].permissions = data.permissions;
-            console.log(data.permissions);
             setTableUsers([...newTableUsers]);
         }
         setEdit(false);
     }
+
+    function handleAddUserClick() {
+        let addUserCard = document.getElementById("tableUsers__addUserCard--id");
+        addUserCard?.classList.toggle("tableUsers__addUserCard--show");
+    }
     
+    async function addUserInTable(){
+        let userId = (document.querySelector(".tableUsers__addUserCard--main input") as HTMLInputElement).value;
+        let response = await fetch(`${url}/graphql`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation($tableId: ID!, $userId: ID!){
+                        addTableUser(tableId: $tableId, userId: $userId){
+                            id
+                            username
+                            email
+                            permissions{
+                                writeEntry
+                                manageRows
+                                manageFields
+                                manageTable
+                                manageUsers
+                            }
+                        }
+                    }
+                `,
+                variables: {
+                    tableId: id,
+                    userId: userId
+                }
+            })
+        });
+        let data = await response.json();
+        if(data.errors){
+            setAddError(data.errors[0].message);
+        }
+        if(data.data.addTableUser){
+            data = data.data.addTableUser;
+            console.log(data);
+            let newTableUsers = [...tableUsers, data];
+            setTableUsers([...newTableUsers]);
+            setAddSuccess("Table user added successfully!" as any);
+
+            setTimeout(()=>{
+                setAddSuccess(null);
+            }, 2000);
+        }
+    }
+
+    async function removeTableUser(userId: string, index: number){
+        let response = await fetch(`${url}/graphql`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation($tableId: ID!, $userId: ID!){
+                        removeTableUser(tableId: $tableId, userId: $userId){
+                            id
+                            username
+                        }
+                    }
+                `,
+                variables: {
+                    tableId: id,
+                    userId: userId
+                }
+            })
+        });
+        let data = await response.json();
+        if(data.errors){
+            setTableUsersError(data.errors[0].message);
+        }
+        if(data.data.removeTableUser){
+            data = data.data.removeTableUser;
+            console.log(data);
+            let newTableUsers = tableUsers;
+            newTableUsers.splice(index, 1);
+            setTableUsers([...newTableUsers]);
+            setTableUsersSuccess("Table user removed successfully!" as any);
+        }
+    }
 
     return (<>
         <div className="tableUsers__main">
@@ -120,17 +213,18 @@ export function TableUsers({url, id}: {url: string, id: string}){
                     {edit ? <> </> : <Pencil width={25} height={25} onClick={()=>{setEdit(true)}} />}
                 </div>
                 <div className="tableUsers__header--add" title="Add a user in table." onClick={()=>{}}>
-                    <UserPlus width={26} height={26} onClick={()=>{}} />
+                    <UserPlus width={26} height={26} onClick={handleAddUserClick} />
                 </div>
             </div>
 
             <div className="tableUsers__content">
                 <div className="tableUsers__users">
                     { tableUsers.map((user: any, index: number)=>{
+                        console.log(index, user.username, user.permissions.writeEntry);
                         return (<div key={index} className="tableUsers__user">
                             <div className="user__actions"> 
                                 {edit && <Save width={18} height={18} onClick={()=>{saveTableUsers(user, index)}} />}
-                                <Trash2 width={18} height={18} onClick={()=>{}} />
+                                <Trash2 width={18} height={18} onClick={()=>{removeTableUser(user.id, index)}} />
                             </div>
                             <div className="user__field">
                                 <div className="user__title">ID</div>
@@ -156,6 +250,7 @@ export function TableUsers({url, id}: {url: string, id: string}){
                                         <Switch switchId="id__userPerm--writeEntry" check={user.permissions.writeEntry} disabled={!edit} switchClass="user__perm--switch" sliderClass="user__perm--slider user__perm--round" onSwitchChange={(e) => {
                                             let newTableUsers = tableUsers; newTableUsers[index].permissions.writeEntry = ((e.target as HTMLInputElement)?.checked); setTableUsers([...newTableUsers]);
                                         }} />
+                                        {user.permissions.writeEntry ? "true" : "false"}
                                     </div>
                                     <div className="user__field">
                                         <div className="user__title">Manage Rows</div>
@@ -186,6 +281,18 @@ export function TableUsers({url, id}: {url: string, id: string}){
                         </div>)
                     })}
                 </div>
+            </div>
+        </div>
+
+        <div className="tableUsers__addUserCard" id="tableUsers__addUserCard--id">
+            <div className="tableUsers__addUserCard--main">
+                <div className="tableUsers__addUserCard--close"><CircleX onClick={handleAddUserClick} /></div>
+                <div className="tableUsers__addUserCard--logo"><Image src="/biz-logo.png" alt="logo" width={80} height={80} /></div>
+                <div className="tableUsers__addUserCard--title">Add a user</div>
+                <input type="number" placeholder="User ID" />
+                {addError && <ErrorBox error={addError} closeError={()=>{setAddError(null)}} />}
+                {addSuccess && <SuccessBox success={addSuccess} closeSuccess={()=>{setAddSuccess(null)}} />}
+                <div className="tableUsers__addUserCard--add" title="Add whitelist email" onClick={addUserInTable}><MailPlus width={26} height={26} /></div>
             </div>
         </div>
 
--- client/src/components/Switch/index.tsx
+++ client/src/components/Switch/index.tsx
@@ -1,15 +1,10 @@
 import "./style.css";
-import { ChangeEvent, useEffect } from "react";
+import { ChangeEvent} from "react";
 
 export function Switch({check, switchId, disabled, switchClass = "switch", sliderClass = "slider round", onSwitchChange = (e)=>{} }: {check: boolean,switchId: string, disabled: boolean, switchClass: string, sliderClass: string, onSwitchChange: (e: ChangeEvent) => void}){
-    useEffect(() => {
-        let switchElement = document.getElementById(switchId) as HTMLInputElement;
-        switchElement.checked = check;
-    }, [check]);
-
     return(<>
         <label className={switchClass}>
-            <input type="checkbox" id={switchId} disabled={disabled} onChange={(e)=>{onSwitchChange(e)}} />
+            <input type="checkbox" id={switchId} disabled={disabled} onChange={(e)=>{onSwitchChange(e)}} checked={check} />
             <span className={sliderClass}></span>
         </label>
     </>)
